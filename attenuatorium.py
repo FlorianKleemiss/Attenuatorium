ce8afb59d4436a117a551f4cfc046c68382edfd2
Florian Kleemiß <florian.kleemiss@uni-bremen.de>
2022-09-09 23:04:34 +0200

    added plots for insight into data quality

--- attenuatorium.py
+++ attenuatorium.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy
 import scipy.stats
 from tkinter import filedialog
+import matplotlib.pyplot as plt
 
 class reflection():
     def __init__(self) -> None:
@@ -73,6 +74,7 @@ class reflection_list():
         self.refl_list = []
         self.min_d = 0.0
         self.max_d = 0.0
+        self.name = ""
     def append_refl(self,ref) -> None:
         if type(ref) != type(reflection):
             print("Mismatch in types!")
@@ -173,6 +175,7 @@ def read_hkl(filename) -> reflection_list:
             if "CELL" in file[n+i]:
                 dump,wl,a,b,c,alpha,beta,gamma = file[n+i].split()
                 listy.set_cell(a,b,c,alpha,beta,gamma,wl)
+    listy.name = os.path.basename(filename)
     return listy
 
 root = tk.Tk()
@@ -188,14 +191,45 @@ intersects = []
 
 for i,file in enumerate(file_paths):
     sets.append(read_hkl(file))
-    file2 = os.path.basename(file)
+    unique_indices = sets[-1].index_set()
+    nr_unique_indices = len(unique_indices)
     nr = sets[i].size()
     ios_counts = sets[i].count_i_to_s_bigger_than((4.0,3.0,2.0,1.0))
-    print(f"{file2:15s} has {nr} reflections, {ios_counts[0]/nr*100:4.1f}%/{ios_counts[1]/nr*100:4.1f}%/{ios_counts[2]/nr*100:4.1f}%/{ios_counts[3]/nr*100:4.1f}% \
-with I/s bigger than 4/3/2/1 d_min: {sets[-1].get_min_d():4.2f} d_max: {sets[-1].get_max_d():4.2f}")
+    print(f"{sets[-1].name:15s} has {nr} reflections, {ios_counts[0]/nr*100:4.1f}%/{ios_counts[1]/nr*100:4.1f}%/{ios_counts[2]/nr*100:4.1f}%/{ios_counts[3]/nr*100:4.1f}% \
+with I/s bigger than 4/3/2/1 d_min: {sets[-1].get_min_d():4.2f} d_max: {sets[-1].get_max_d():4.2f}; {nr_unique_indices:4d} unique indices with avg. \
+{nr/nr_unique_indices:5.3f} redundancy")
     hkls.append(sets[-1].index_set())
     if i > 0:
         intersects.append(hkls[i-1].intersection(hkls[i]))
+
+fig,axes = plt.subplots(2,1)
+axes[0].invert_xaxis()
+colors = ['b','g','r','y']
+a = []
+names = []
+axes[0].set_xlabel("d /Angs")
+axes[1].set_xlabel("I/sigma")
+axes[0].set_ylabel("I")
+axes[1].set_ylabel("# refl.")
+for i,s in enumerate(sets):
+    x = []
+    y = []
+    i_o_s = []
+    names.append(s.name)
+    for ref in s.refl_list:
+        x.append(s.cell.get_d_of_hkl(ref.index()))
+        y.append(ref.i())
+        i_o_s.append(ref.i_over_s())
+    axes[0].scatter(x,y,s=10,facecolors='none',edgecolors=colors[i],label=names[-1])
+    a.append(np.array(i_o_s))
+    
+    #axes.scatter(x,b_result,s=10,facecolors='none',edgecolors='g',label="b")
+    #axes.scatter(x,c_result,s=10,facecolors='none',edgecolors='r',label="c")
+    #axes.scatter(x,d_result,s=10,facecolors='none',edgecolors='y',label="d")
+    #axes.plot(x,g_result,'--',label="f_table1")
+axes[1].hist(a,bins=np.linspace(0,50,25), color=colors, label=names)
+plt.legend(loc="upper right")
+plt.show()
 
 
 print("OK")
