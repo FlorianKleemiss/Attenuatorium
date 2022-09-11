d0c74505dd882627de87f67302d56020b53cdef3
Florian Kleemiß <florian.kleemiss@uni-bremen.de>
2022-09-11 22:07:54 +0200

    switched over to numpy

--- attenuatorium.py
+++ attenuatorium.py
@@ -12,8 +12,8 @@ class reflection():
         self.ind = (0,0,0)
         self.intensity = 0.0
         self.sigma = 0.0
-    def __init__(self, h, k, l, i, s) -> None:
-        self.ind = (h,k,l)
+    def __init__(self, hkl, i, s) -> None:
+        self.ind = hkl
         self.intensity = i
         self.sigma = s
     def l(self) -> int:
@@ -64,43 +64,36 @@ class cell():
               + 2.0 * hkl[0] * hkl[2] / (self.a * self.c) * (self.cg * self.ca - self.cb) \
               + 2.0 * hkl[0] * hkl[1] / (self.a * self.b) * (self.ca * self.cb - self.cg)
         lower = 1 - pow(self.ca, 2) - pow(self.cb, 2) - pow(self.cg, 2) + 2 * self.ca * self.cb * self.cg
-        d = math.sqrt(lower / upper)
+        d = np.sqrt(lower / upper)
         return d
     def get_stl_of_hkl(self,hkl) -> float:
         return 1.0 / (2 * self.get_d_of_hkl(hkl))
 
 class reflection_list():
     def __init__(self) -> None:
-        self.refl_list = []
+        self.refl_list = np.array([[],[],[]])
         self.min_d = 0.0
         self.max_d = 0.0
         self.name = ""
-    def append_refl(self,ref) -> None:
-        if type(ref) != type(reflection):
-            print("Mismatch in types!")
-            exit(-1)
-        self.refl_list.append(ref)
     def append(self, h, k, l, i, s) -> None:
-        self.refl_list.append(reflection(h,k,l,i,s))
+        t = np.array([[(h,k,l),i,s]],dtype=object)
+        self.refl_list = np.append(self.refl_list,t.transpose(),axis=1)
     def index_tuple(self) -> tuple:
-        ret = []
-        for ref in self.refl_list:
-            ret.append(tuple(ref.index()))
-        return tuple(ret)
-    def index_set(self) -> set:
+        return self.refl_list[0]
+    def unique_hkl(self) -> set:
         li = self.index_tuple()
-        ret = set(li)
+        ret = np.unique(li)
         return ret
-    def get_hkl(self,h,k,l) -> list:
+    def get_hkl(self,hkl) -> list:
         ret = []
-        for ref in self.refl_list:
-            if ref.h() == h and ref.k() == k and ref.l() == l:
-                ret.append(ref)
+        #condition = np.array(self.refl_list[0]==hkl)
+        condition = np.array([x == hkl for x in self.refl_list[0]])
+        ret = np.compress(condition,self.refl_list,axis=1)
         return ret
-    def has_hkl(self,h,k,l) -> bool:
+    def has_hkl(self,hkl) -> bool:
         ret = False
         for ref in self.refl_list:
-            if ref.h() == h and ref.k() == k and ref.l() == l:
+            if ref.index() == hkl:
                 ret = True
         return ret
     def get_subset(self, hkl_list):
@@ -116,43 +109,34 @@ class reflection_list():
                 ret.append(ref)
         return ret
     def count_i_to_s_bigger_than(self, cutoff) -> int:
-        ret = 0
-        for ref in self.refl_list:
-            if ref.i_over_s() > cutoff:
-                ret+=1
+        ret = np.count_nonzero(self.refl_list[1]/self.refl_list[2] > cutoff)
         return ret
     def count_i_to_s_bigger_than(self, cutoff) -> int:
         if type(cutoff) == type(int):
-            ret = 0
-            for ref in self.refl_list:
-                if ref.i_over_s() > cutoff:
-                    ret+=1
+            ret = np.count_nonzero(self.refl_list[1]/self.refl_list[2] > cut)
         if type(cutoff) == type([]) or type(cutoff) == type(()):
             ret = []
             for cut in cutoff:
-                ret.append(0)
-                for ref in self.refl_list:
-                    if ref.i_over_s() > cut:
-                        ret[-1]+=1
+                ret.append(np.count_nonzero(abs(self.refl_list[1]/self.refl_list[2]) > cut))
         return ret
     def size(self) -> int:
-        return len(self.refl_list)
+        return self.refl_list[0].size
     def set_cell(self,a,b,c,alpha,beta,gamma, wl) -> None:
         self.cell = cell(a,b,c,alpha,beta,gamma,wl)
         mind = 999.0
         maxd = 0.0
-        for ref in self.refl_list:
-            d = self.cell.get_d_of_hkl(ref.index())
-            if d < mind:
-                mind = d
-            if d > maxd:
-                maxd = d
+        d_func = np.vectorize(self.cell.get_d_of_hkl)
+        d = d_func(self.refl_list[0])
+        mind = np.min(d)
+        maxd = np.max(d)
         self.max_d = maxd
         self.min_d = mind
     def get_min_d(self):
         return self.min_d
     def get_max_d(self):
         return self.max_d
+    def get_intersection_indices(self, given):
+        return np.intersect1d(self.refl_list[0],given.refl_list[0])
 
 
 
@@ -170,10 +154,10 @@ def read_hkl(filename) -> reflection_list:
               float(line[13:20]),
               float(line[21:28])
             )
-    if i < len(file) - 1:
-        for n in range(len(file)-i):
-            if "CELL" in file[n+i]:
-                dump,wl,a,b,c,alpha,beta,gamma = file[n+i].split()
+    if last_line < len(file) - 1:
+        for n in range(len(file)-last_line):
+            if "CELL" in file[n+last_line]:
+                dump,wl,a,b,c,alpha,beta,gamma = file[n+last_line].split()
                 listy.set_cell(a,b,c,alpha,beta,gamma,wl)
     listy.name = os.path.basename(filename)
     return listy
@@ -188,25 +172,48 @@ number_of_file = len(file_paths)
 sets = []
 hkls = []
 intersects = []
+colors = ['b','g','r','y']
+names = []
 
 for i,file in enumerate(file_paths):
     sets.append(read_hkl(file))
-    unique_indices = sets[-1].index_set()
-    nr_unique_indices = len(unique_indices)
-    nr = sets[i].size()
-    ios_counts = sets[i].count_i_to_s_bigger_than((4.0,3.0,2.0,1.0))
+    names.append(sets[-1].name)
+    unique_indices = sets[-1].unique_hkl()
+    nr_unique_indices = unique_indices.size
+    nr = sets[-1].size()
+    ios_counts = sets[-1].count_i_to_s_bigger_than((4.0,3.0,2.0,1.0))
     print(f"{sets[-1].name:15s} has {nr} reflections, {ios_counts[0]/nr*100:4.1f}%/{ios_counts[1]/nr*100:4.1f}%/{ios_counts[2]/nr*100:4.1f}%/{ios_counts[3]/nr*100:4.1f}% \
 with I/s bigger than 4/3/2/1 d_min: {sets[-1].get_min_d():4.2f} d_max: {sets[-1].get_max_d():4.2f}; {nr_unique_indices:4d} unique indices with avg. \
 {nr/nr_unique_indices:5.3f} redundancy")
-    hkls.append(sets[-1].index_set())
+    hkls.append(unique_indices)
     if i > 0:
-        intersects.append(hkls[i-1].intersection(hkls[i]))
+        intersects.append(sets[-2].get_intersection_indices(sets[-1]))
+        _x = []
+        _y = []
+        for hkl in intersects[-1]:
+            refs1 = sets[-2].get_hkl(hkl)
+            refs2 = sets[-1].get_hkl(hkl)
+            av1 = 0
+            size1 = refs1.shape[1]
+            for r in range(size1):
+                av1 += refs1[1][r]
+            av1 /= size1
+            _x.append(av1)
+            av2 = 0
+            size2 = refs2.shape[1]
+            for r in range(size2):
+                av2 += refs2[1][r]
+            av2 /= len(refs2)
+            _y.append(av2)
+        plt.scatter(_x,_y,s=5,facecolors='none',edgecolors=colors[i],label=f"{sets[-2].name} vs {sets[-1].name}")
+plt.xlabel(f"I(more attenuation)")
+plt.ylabel(f"I(less attenuation)")
+plt.legend(loc="upper left")
+plt.show()
 
 fig,axes = plt.subplots(2,1)
 axes[0].invert_xaxis()
-colors = ['b','g','r','y']
 a = []
-names = []
 axes[0].set_xlabel("d /Angs")
 axes[1].set_xlabel("I/sigma")
 axes[0].set_ylabel("I")
@@ -215,19 +222,20 @@ for i,s in enumerate(sets):
     x = []
     y = []
     i_o_s = []
-    names.append(s.name)
     for ref in s.refl_list:
         x.append(s.cell.get_d_of_hkl(ref.index()))
         y.append(ref.i())
         i_o_s.append(ref.i_over_s())
-    axes[0].scatter(x,y,s=10,facecolors='none',edgecolors=colors[i],label=names[-1])
+    axes[0].scatter(x,y,s=5,facecolors='none',edgecolors=colors[i],label=names[i])
     a.append(np.array(i_o_s))
     
     #axes.scatter(x,b_result,s=10,facecolors='none',edgecolors='g',label="b")
     #axes.scatter(x,c_result,s=10,facecolors='none',edgecolors='r',label="c")
     #axes.scatter(x,d_result,s=10,facecolors='none',edgecolors='y',label="d")
     #axes.plot(x,g_result,'--',label="f_table1")
-axes[1].hist(a,bins=np.linspace(0,50,25), color=colors, label=names)
+upper = int(max(np.amax(x) for x in a))
+steps = int(upper/2)
+axes[1].hist(a,bins=np.linspace(0,upper,steps), color=colors, label=names)
 plt.legend(loc="upper right")
 plt.show()
 
